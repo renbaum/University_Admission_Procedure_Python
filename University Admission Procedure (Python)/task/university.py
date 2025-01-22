8ee44e4fb7e37f6ed603a6aad3012191df25741d
renbaum <renbaum04@bluewin.ch>
2025-01-22 10:00:04 +0100

    Initial commit

---
+++ University Admission Procedure (Python)/task/university.py
@@ -0,0 +1,9 @@
+
+
+if __name__ == "__main__":
+    score1 = int(input())
+    score2 = int(input())
+    score3 = int(input())
+    average = (score1 + score2 + score3) / 3
+    print(average)
+    print("Congratulations, you are accepted!")
